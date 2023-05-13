275aa15a2da343ef4d8957ccb9017568dd94f157
Fabian Polkehn <fabian.polkehn@iu.org>
2023-05-14 01:23:46 +0200

    integrate PHP Insights

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -24,7 +24,7 @@ class Handler extends ExceptionHandler
     public function register(): void
     {
         $this->reportable(function (Throwable $e) {
-            //
+            unset($e);
         });
     }
 }
--- app/Models/User.php
+++ app/Models/User.php
@@ -21,6 +21,7 @@ use Laravel\Sanctum\HasApiTokens;
  * @property string|null $remember_token
  * @property \Illuminate\Support\Carbon|null $created_at
  * @property \Illuminate\Support\Carbon|null $updated_at
+ *
  * @property-read \Illuminate\Notifications\DatabaseNotificationCollection<int, \Illuminate\Notifications\DatabaseNotification> $notifications
  * @property-read int|null $notifications_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \App\Models\Session> $ownedSessions
@@ -29,6 +30,7 @@ use Laravel\Sanctum\HasApiTokens;
  * @property-read int|null $sessions_count
  * @property-read \Illuminate\Database\Eloquent\Collection<int, \Laravel\Sanctum\PersonalAccessToken> $tokens
  * @property-read int|null $tokens_count
+ *
  * @method static \Database\Factories\UserFactory factory($count = null, $state = [])
  * @method static \Illuminate\Database\Eloquent\Builder|User newModelQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|User newQuery()
@@ -41,6 +43,7 @@ use Laravel\Sanctum\HasApiTokens;
  * @method static \Illuminate\Database\Eloquent\Builder|User wherePassword($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereRememberToken($value)
  * @method static \Illuminate\Database\Eloquent\Builder|User whereUpdatedAt($value)
+ *
  * @mixin \Eloquent
  */
 class User extends Authenticatable implements MustVerifyEmail
@@ -100,7 +103,6 @@ class User extends Authenticatable implements MustVerifyEmail
         // @TODO: Implement
         return true;
     }
-
 
     public function sendEmailVerificationNotification(): void
     {
