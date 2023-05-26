2357ceaa976d91b808811cba453c91324a9b8a09
Andreas Halemba ext <a.halemba.ext@careerpartner.eu>
2023-05-26 23:34:47 +0200

    🐞 fix: stan issues

--- tests/Feature/ExampleTest.php
+++ tests/Feature/ExampleTest.php
@@ -1,6 +1,7 @@
 <?php
 
-it('returns a successful response', function () {
-    $response = $this->get('/');
-    $response->assertStatus(302);
+use function Pest\Laravel\get;
+
+it('redirects to dashboard', function () {
+    get('/')->assertRedirectContains('dashboard');
 });
