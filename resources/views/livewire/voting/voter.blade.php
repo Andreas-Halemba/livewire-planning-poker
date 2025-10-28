4e0c65a62880bf18ec224921e6c9d14516cf0717
Andreas Halemba <andreas.halemba@iu.org>
2025-10-28 17:41:26 +0100

    refactor: Simplify HTML structure in voter.blade.php
    - Clean up the HTML structure by removing unnecessary line breaks and indentation in the voting and done issues sections.
    - Maintain existing functionality while improving code readability and maintainability.

--- resources/views/livewire/voting/voter.blade.php
+++ resources/views/livewire/voting/voter.blade.php
@@ -2,17 +2,14 @@
     <div class="grid gap-3 p-4 rounded-lg bg-base-300">
         <h2 class="order-first text-xl">Issues</h2>
         @foreach ($session->issues->where('status', '!=', 'finished') as $issue)
-            <div
-                tabindex="0"
-                @class([
-                    'border border-base-300 bg-base-100 rounded-box text-base-content',
-                    'collapse-plus collapse' => !blank($issue->description),
-                    '!border-success order-first mb-10 !collapse-open' => $issue->isVoting(),
-                    '!border-neutral order-last' => $issue->isFinished(),
-                    'border-yellow-500',
-                ])
-                wire:key="issue-{{ $issue->id }}"
-            >
+            <div tabindex="0" @class([
+                'border border-base-300 bg-base-100 rounded-box text-base-content',
+                'collapse-plus collapse' => !blank($issue->description),
+                '!border-success order-first mb-10 !collapse-open' => $issue->isVoting(),
+                '!border-neutral order-last' => $issue->isFinished(),
+                'border-yellow-500',
+            ])
+                wire:key="issue-{{ $issue->id }}">
                 <div class="text-xl font-medium collapse-title">
                     <div class="flex flex-col gap-1">
                         @if ($issue->isVoting())
@@ -36,18 +33,14 @@
     <div class="grid gap-3 p-4 mt-10 rounded-lg bg-base-300">
         <h2 class="text-xl">Done issues</h2>
         @foreach ($session->issues->where('status', 'finished') as $issue)
-            <div
-                tabindex="0"
-                @class([
-                    'border order-last bg-base-100 text-base-content !border-neutral rounded-box',
-                    'collapse-plus collapse' => !blank($issue->description),
-                ])
-                wire:key="issue-{{ $issue->id }}"
-            >
+            <div tabindex="0" @class([
+                'border order-last bg-base-100 text-base-content !border-neutral rounded-box',
+                'collapse-plus collapse' => !blank($issue->description),
+            ]) wire:key="issue-{{ $issue->id }}">
                 <div class="text-xl font-medium collapse-title">
                     <div class="flex items-center gap-4">
                         <span class="w-10 h-5 badge badge-success">{{ $issue->storypoints }}</span>
-                        <span >{!! $issue->title_html !!}</span>
+                        <span>{!! $issue->title_html !!}</span>
                     </div>
                 </div>
                 @unless (blank($issue->description))
