a7d68782c5040db8aa87543be7d20c639d2e5627
Andreas Halemba <andreas.halemba@iu.org>
2025-10-30 20:36:58 +0100

    fix: Update delete user form input IDs for clarity
    - Change input label and ID from 'password' to 'delete_password' in the user deletion form for better context and to avoid confusion.

--- resources/views/profile/partials/delete-user-form.blade.php
+++ resources/views/profile/partials/delete-user-form.blade.php
@@ -26,9 +26,9 @@
             </p>
 
             <div class="mt-6 flex flex-col gap-2">
-                <x-input-label for="password" value="{{ __('Password') }}" class="sr-only" />
+                <x-input-label for="delete_password" value="{{ __('Password') }}" class="sr-only" />
 
-                <x-text-input id="password" name="password" type="password" placeholder="{{ __('Password') }}" />
+                <x-text-input id="delete_password" name="password" type="password" placeholder="{{ __('Password') }}" />
 
                 <x-input-error :messages="$errors->userDeletion->get('password')" />
             </div>
