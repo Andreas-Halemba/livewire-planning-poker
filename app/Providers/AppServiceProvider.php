7e4e8225ec78aab26347cfa0d140c865024f5fa2
Andreas Halemba <andreas.halemba@iu.org>
2025-11-28 15:19:03 +0100

    feat: implement viewPulse gate for user access control
    - Defined a new Gate named 'viewPulse' to control access based on the environment and user email.
    - Enhanced the AppServiceProvider to include the new authorization logic for improved security and user management.

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Models\User;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -17,5 +19,8 @@ class AppServiceProvider extends ServiceProvider
     /**
      * Bootstrap any application services.
      */
-    public function boot(): void {}
+    public function boot(): void
+    {
+        Gate::define('viewPulse', fn(User $user) => app()->environment('local') || $user->email === 'andreas.halemba@iu.org');
+    }
 }
